c0bab85b977be0abc1cd431963b865eddf0ba1dc
patrikklimko <patrik.klimko123@gmail.com>
2025-05-03 21:14:28 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // filepath: c:\Users\patri\Pokedex\pokedex\vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/pokedex/',  
+  base: '/pokedex/', // Ensure this is set for GitHub Pages
   plugins: [react()],
-})
+});

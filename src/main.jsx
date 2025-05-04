52c150c266afeadf65ce3f93efebc44653e37605
patrikklimko <patrik.klimko123@gmail.com>
2025-05-04 11:09:03 +0200

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/App.jsx';
+import App from './App.jsx';
 import { HashRouter } from 'react-router-dom';
-import './src/index.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
